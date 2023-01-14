e371712edbd0bc6ad177ca99cea06f21fd41b975
Renne Castro <rcastro2@schools.nyc.gov>
2023-01-14 03:21:19 -0500

    adds example using chinook.db

---
+++ ChinookExplore.java
@@ -0,0 +1,32 @@
+
+import java.io.IOException;
+
+import java.sql.*;
+//javac -cp sqlite-jdbc-3.23.1.jar; DatabaseExplore.java
+
+public class ChinookExplore {
+
+    public static void main(String[] args) throws IOException {
+        
+        Database db = new Database("jdbc:sqlite:chinook.db");
+        
+        String sql;
+        
+        sql  = "SELECT * FROM artists " + 
+               "WHERE Name = 'House Of Pain'";
+        System.out.println(db.runSQL(sql,"table-auto"));
+
+        sql  = "SELECT albums.Albumid, artists.Name FROM albums " + 
+               "INNER JOIN artists ON artists.ArtistId = albums.ArtistId " +
+               "WHERE Name = 'House Of Pain'";
+        System.out.println(db.runSQL(sql,"table-auto"));
+
+        sql = "SELECT tracks.TrackId, tracks.Name FROM tracks " + 
+              "INNER JOIN albums ON albums.AlbumId = tracks.AlbumId " +
+              "INNER JOIN artists ON artists.ArtistId = albums.ArtistId " +
+              "WHERE artists.Name = 'House Of Pain'";
+        System.out.println(db.runSQL(sql,"table-auto"));
+       
+    }    
+
+}
